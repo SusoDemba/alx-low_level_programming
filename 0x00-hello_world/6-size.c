b888958a3a9e8a43e7740f1d1d49702116040a94
SusoDemba <susodemba491@gmail.com>
2022-12-14 16:11:05 +0000

    prints the size of each variable type in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,16 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a float: %lu byte(s) \n" (unsigned long)sizeof(long long int));
+	char a;
+	int b;
+	float c;
+	long int d;
+	long long int e;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %lu byte(s) \n", (unsigned long)sizeof(e));
 	return (0);
 }
