e3bb0703ccac1b3ae048c27fe5172a5022d833b7
SusoDemba <susodemba491@gmail.com>
2022-12-15 17:26:16 +0000

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  * variable num is initialized to 0
- * each time the loop condition is true, it prints the content in num,
+ * each time the loop condition is true,
+ * it prints the content in num,
  * and increement num by 1
  * Then prints the new line
- * main function returns 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	short num = '0';
+
 	while (num <= '9')
 	{
 		putchar(num);
 		num++;
 	}
 	putchar('\n');
+
+	return (0);
 }
