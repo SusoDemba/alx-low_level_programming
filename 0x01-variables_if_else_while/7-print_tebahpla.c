d62f2bcacb7c447481bae8c6b0edab3de314fe13
SusoDemba <susodemba491@gmail.com>
2022-12-15 17:30:07 +0000

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  * variable letter is initialized to z,
  * while letter >= a, content in letter is printed and
  * letter is decrements by 1
- * main returns 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char letter;
-	
+
 	letter = 'z';
 	while (letter >= 'a')
 	{
@@ -17,4 +18,6 @@ int main(void)
 		letter--;
 	}
 	putchar('\n');
+
+	return (0);
 }
