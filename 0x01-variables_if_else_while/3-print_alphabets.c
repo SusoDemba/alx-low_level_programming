8d245aed803aaf2160b741619a0696826907a0ec
SusoDemba <susodemba491@gmail.com>
2022-12-15 17:07:20 +0000

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * main: Entry point
+ * main - Entry point
  * letter is a variable that contains one letter at a time successively
  * The first for loop prints small letters from a - z
- * The second for loop prints capital letters from A - Z on the same line as the first
- * Then the function returns 0
+ * The second for loop prints capital letters from A - Z
+ * on the same line as the first
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	for (letter = 'A'; letter <= 'Z'; letter++)
