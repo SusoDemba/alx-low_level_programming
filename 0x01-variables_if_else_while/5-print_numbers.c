5037c72dafad89600973529b9897aa947fa54c4f
SusoDemba <susodemba491@gmail.com>
2022-12-15 17:20:15 +0000

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  * num variable is initialized to 0
- * If num is less than 10, content in num is diaplayed and 
+ * If num is less than 10, content in num is diaplayed and
  * after each loop, num increments by 1
- * main returns 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	short num = 0;
+
 	while (num < 10)
 	{
 		printf("%d", num);
