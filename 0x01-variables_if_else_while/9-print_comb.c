e15292a66e75a602719235ddeb61cb760264f1e9
SusoDemba <susodemba491@gmail.com>
2022-12-15 18:06:31 +0000

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,14 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		putchar(',');
-		if (num != 9)
+		if (num != '9')
+			putchar(',');
 			putchar(' ');
 	}
+	putchar('\n');
 
 	return (0);
 }
