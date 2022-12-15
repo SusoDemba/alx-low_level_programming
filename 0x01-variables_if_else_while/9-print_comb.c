8c4a657ef79b949fab348e15ddcd2377758c2513
SusoDemba <susodemba491@gmail.com>
2022-12-15 17:56:49 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * variable num is initialized to 0, and
+ * it increments by 1 after each iteration.
+ * During every iteration, print the content in num.
+ * if content in num <= 9(last valid number),
+ * print comma(,) after it.
+ * Otherwise, if it's 9, do nothing and terminate the loop
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+		putchar(',');
+		if (num != 9)
+			putchar(' ');
+	}
+
+	return (0);
+}
