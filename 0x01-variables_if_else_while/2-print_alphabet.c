ed54d70d1ae2106b2da74ccc445d9c449925a733
SusoDemba <susodemba491@gmail.com>
2022-12-15 16:45:01 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- * At first, variable letter point at a, then for each iteration, it points at the next value
+ * main - Entry point
+ * At first, variable letter point at a,
+ * then for each iteration, it points at the next value
  * After the value z, the condition fails,
  * and new line is made
  * Then the main fuction returns 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
