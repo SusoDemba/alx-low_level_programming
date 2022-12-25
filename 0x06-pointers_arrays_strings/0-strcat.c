173fa690c16e0fa8ad276ba19c817f6bbaea4f7a
SusoDemba <susodemba491@gmail.com>
2022-12-25 21:57:11 +0000

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * length_of_string - calculates the number of
-   elements in an string
- * @s: the string which length is to be determined
- * Return: length of the string
- */
-int length_of_string(char *s)
-{
-	int length;
-
-	for (length = 0; *s != '\0'; s++)
-		length++;
-
-	return (length);
-}
-
-/**
  * *_strcat - concatenate two strings
  * @src: string that it appended to
  * @dest: appends the src string to itself
@@ -26,22 +10,18 @@ int length_of_string(char *s)
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
+	int j = 0;
+	int idx = 0;
 
-	while (*dest != '\0')
-		dest++;
-	while (*src != '\0')
+	while (dest[j] != '\0')
+		j++;
+	while (src[i] != '\0')
 	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
-	*dest = '\0';
-
-	while (i <= length_of_string(dest) && i >= 0)
-	{
-		dest--;
+		dest[j] = src[i];
 		i++;
+		j++;
 	}
+	dest[j] = '\0';
 
 	return (dest);
 }
