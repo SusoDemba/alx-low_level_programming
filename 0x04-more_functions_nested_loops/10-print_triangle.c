daab689843b3c53ded0bc2d0e1d64fed15475379
SusoDemba <susodemba491@gmail.com>
2022-12-27 15:34:17 +0000

    programs on C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,7 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
 	if (size <= 0)
 	{
@@ -17,10 +18,14 @@ void print_triangle(int size)
 	}
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 1; j <= size - i; j++)
+			_putchar(' ');
+
+		for (k = 1; k <= i; k++)
 			_putchar('#');
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 
 	return;
 }
