6db2906042ecc6ebce4c0146a2bed8f3bc52d757
SusoDemba <susodemba491@gmail.com>
2022-12-27 15:44:41 +0000

    programs on C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,14 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		if (size <= 0)
-			break;
-		for (j = 0; j < size; j++)
-			_putchar('#');
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+		}
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
